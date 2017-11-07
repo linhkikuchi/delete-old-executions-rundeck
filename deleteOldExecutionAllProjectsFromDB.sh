c5c78f5a7076ab58c8ccb304c06c08587c716b84
Linh <xuongrongl84@gmail.com>
2017-11-07 13:36:47 +1300

    Update deleteOldExecutionAllProjectsFromDB.sh

--- deleteOldExecutionAllProjectsFromDB.sh
+++ deleteOldExecutionAllProjectsFromDB.sh
@@ -22,10 +22,10 @@ JOBS=`find . -maxdepth 4 -path "*/job/*/logs" -type d`
                      echo "   rm -rf $j/$job.*"
                      rm -rf $j/$job.*
                      echo "   mysql -u rundeck -p 'delete from execution where id=$job'"
-                     mysql -u rundeck -p[8}w}lWs rundeck -e "delete from execution where id=$job"
+                     mysql -u rundeck -ppassword rundeck -e "delete from execution where id=$job"
                      echo "   mysql -u rundeck -p -c 'delete from base_report where jc_exec_id=${job}'"
-                     mysql -u rundeck -p[8}w}lWs rundeck -e "delete from base_report where jc_exec_id=${job}"
-                     mysql -u rundeck -p[8}w}lWs rundeck -e "commit"
+                     mysql -u rundeck -ppassword rundeck -e "delete from base_report where jc_exec_id=${job}"
+                     mysql -u rundeck -ppassword rundeck -e "commit"
                   done
                 fi
         done
